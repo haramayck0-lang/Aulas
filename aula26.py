c5dc5adb08bae2715e3f204301c324d078db534f
haramayck0-lang <haramayck0@gmail.com>
2026-02-18 14:05:19 -0400

    Aulas 24,25,26

---
+++ aula26.py
@@ -0,0 +1,15 @@
+"""
+Flag (Bandeira) - Marca um local
+None = Não valor
+is e is not = é ou não é (tipo, identidade)
+id = identidade
+"""
+
+condicao = True
+passou_no_if = False
+
+if condicao:
+    print('Verdadeiro')
+else:
+    print('Falso') 
+print(passou_no_if)
